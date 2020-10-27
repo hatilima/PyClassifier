1104363be7b7d64813f57f3bd5c90cbc50a86147
hatilima <jasperhatilima@gmail.com>
2020-10-27 12:44:39 +0100

    changing master from initial commit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print("Hello world!")
 print("Another line just for Vi")
 print("Master Ended here!")
-
+print("We add another line to master!")
 # We will need to import torch at some point. So this is a comment on master branch.
